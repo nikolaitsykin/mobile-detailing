fcb5c4f39efdc525101d2a3d3d2f8c46992cd527
flipexe <104075088+flipexe@users.noreply.github.com>
2024-09-11 06:22:19 -0400

    scroll element removed from header

--- src/utils/scrollToTop.js
+++ src/utils/scrollToTop.js
@@ -7,43 +7,16 @@ const ScrollToTop = () => {
   // Extracts pathname property(key) from an object
   const { pathname } = useLocation();
 
-  const [isVisible, setIsVisible] = useState(false);
-
-  const toggleVisibility = () => {
-    window.scrollY > 200 ? setIsVisible(true) : setIsVisible(false);
-  };
-
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
-  // Toggles visibility
-  useEffect(() => {
-    window.addEventListener("scroll", toggleVisibility);
-    return () => {
-      window.removeEventListener("scroll", toggleVisibility);
-    };
-  }, []);
-
   // Automatically scrolls to top whenever pathname changes
   useEffect(() => {
-    window.scrollTo(0, 0);
+    scrollToTop();
   }, [pathname]);
 
-  return (
-    <div className="z-50 fixed bottom-10 right-20">
-      <button
-        type="button"
-        onClick={scrollToTop}
-        className={classNames(
-          isVisible ? "opacity-100" : "opacity-0",
-          "bg-pink-600 hover:bg-pink-700 focus:ring-pink-500 inline-flex items-center rounded-full p-3 text-white shadow-sm transition-opacity focus:outline-none focus:ring-2 focus:ring-offset-2"
-        )}
-      >
-        <FaAngleDoubleUp size="2rem" color="fffafa" />
-      </button>
-    </div>
-  );
+  return <div className=""></div>;
 };
 
 export default ScrollToTop;
