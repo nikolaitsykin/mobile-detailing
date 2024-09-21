9a37df2c631bd149f00445b7af8cf6824465cd59
flipexe <104075088+flipexe@users.noreply.github.com>
2024-09-21 14:22:15 -0400

    booking buttons marging fix

--- src/components/UI/BookButtons.jsx
+++ src/components/UI/BookButtons.jsx
@@ -5,7 +5,7 @@ import Button from "./Button";
 const BookButtons = ({ color }) => {
   return (
     <div className="py-5 flex  justify-center items-center">
-      <div className="my-1 min-w-40 md:min-w-48">
+      <div className="m-1 min-w-40 md:min-w-48">
         <Link
           to="https://squareup.com/appointments/book/t25pubegvfjcw1/LDK47FK18Z9GH/start"
           target="_blank"
@@ -13,7 +13,7 @@ const BookButtons = ({ color }) => {
           <Button children={"Book Online"} color={color} />
         </Link>
       </div>
-      <div className="my-1 min-w-40 md:min-w-48">
+      <div className="m-1 min-w-40 md:min-w-48">
         <Link to="/booking">
           <Button children={"Request Quote"} color={color} />
         </Link>
