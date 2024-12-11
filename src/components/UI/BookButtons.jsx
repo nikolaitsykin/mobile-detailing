d1c92f4d59861a244f1567d19fd02786f4aaae3f
flipexe <104075088+flipexe@users.noreply.github.com>
2024-12-11 15:14:54 -0500

    location fix

--- src/components/UI/BookButtons.jsx
+++ src/components/UI/BookButtons.jsx
@@ -5,13 +5,13 @@ import { BOOKING_URL, CONTACT_PATH } from "../../utils/constants";
 
 const BookButtons = ({ color }) => {
   return (
-    <div className="py-5 flex flex-col xxs:flex-row justify-center items-center">
-      <div className="m-1 min-w-40 md:min-w-48">
+    <div className="flex flex-col xxs:flex-row justify-center items-center">
+      <div className="m-0.5 min-w-32 md:w-40">
         <Link to={BOOKING_URL} target="_blank">
           <Button children={"Book Online"} color={color} />
         </Link>
       </div>
-      <div className="m-1 min-w-40 md:min-w-48">
+      <div className="m-0.5 min-w-32 md:w-40">
         <Link to={CONTACT_PATH}>
           <Button children={"Request Quote"} color={color} />
         </Link>
