70a69535c9ba532c3ed825db7e25d9b262eb42fa
flipexe <104075088+flipexe@users.noreply.github.com>
2025-06-22 07:13:19 -0400

    Phone call link added

--- src/components/UI/ConntactsButtons.jsx
+++ src/components/UI/ConntactsButtons.jsx
@@ -1,20 +1,18 @@
 import { Link } from "react-router-dom";
 import { ReactComponent as MessageSms } from "../../assets/icons/message-sms.svg";
 import { ReactComponent as PhoneCall } from "../../assets/icons/phone-call.svg";
-import {
-  BOOKING_URL,
-  CALL_NUMBER,
-  TEXT_NUMBER
-} from "../../utils/constants";
+import { BOOKING_URL, CALL_NUMBER, TEXT_NUMBER } from "../../utils/constants";
 import Button from "./Button";
 
 const ConntactsButtons = () => {
   return (
     <div className="flex h-12 gap-2">
       <div className="md:flex hidden justify-center items-center px-1">
-        <span className="text-3xl font-semibold text-white font-stretch-ultra-condensed ">
-          919-906-0099
-        </span>
+        <Link to={CALL_NUMBER}>
+          <span className="text-3xl font-semibold text-white font-stretch-ultra-condensed ">
+            919-906-0099
+          </span>
+        </Link>
       </div>
       <div className="flex md:hidden justify-center items-center active:scale-110 duration-300 ">
         <Link to={CALL_NUMBER}>
